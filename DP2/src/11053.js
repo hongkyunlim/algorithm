3a4f40973e019808b483c8d94ab1bd270f4d0112
HONGKYUN <daru87@ex-em.com>
2019-06-04 00:14:34 +0900

    [algorithm] DP2
    DP2

---
+++ DP2/src/11053.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var inputs = require('fs').readFileSync(path.resolve('./11057.txt'), 'utf8').toString().trim().split('\n');
+// var inputs = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+var n = +(inputs[0].split(' ')[0].trim());
+var a = inputs[1].split(' ');
+var max = 0;
+var d = [];
+
+for (var ix = 0; ix < n; ix++) {
+    d[ix] = 1;
+    for(var jx = 0; jx < ix; jx++) {
+        if (+a[jx] < +a[ix] && d[ix] < d[jx] + 1) {
+            d[ix] = d[jx] + 1;
+        }
+    }
+    max = Math.max(d[ix], max);
+}
+
+console.log(max);
